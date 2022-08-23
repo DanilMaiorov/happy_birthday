ae52b0c18437bde5d3f5a202bebfcb70889a6967
Danil Maiorov <damayorov93@gmail.com>
2022-08-23 18:19:41 +0300

    second day

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log('hello');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+
+const changeToMen = () => {
+    body.classList.add('men');
+    body.classList.remove('woman');
+};
+const changeTowoman = () => {
+    body.classList.add('woman');
+    body.classList.remove('men');
+};
+
+header.addEventListener('click', (e) => {
+    if(e.target.classList.contains('header__button-change_male')) {
+        if(body.classList.contains('woman')) {
+            changeToMen();
+        }
+    }
+    if(e.target.classList.contains('header__button-change_female')) {
+        if(body.classList.contains('men')) {
+            changeTowoman();
+        }
+    }
+});
